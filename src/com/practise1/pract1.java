eedaad2a9bd9ac8de8678052ad08395cc4b6da6d
sumanth1231807 <sumanthravi11111@gmail.com>
2022-03-20 18:44:17 +0530

    pract1

---
+++ src/com/practise1/pract1.java
@@ -0,0 +1,11 @@
+package com.practise1;
+
+public class pract1 {
+    public static void main(String[] args) {
+        int num1=10,num2=20,sum=0;
+        System.out.println("num1="+num1);
+        System.out.println("num1="+num1);
+    }
+
+
+}
